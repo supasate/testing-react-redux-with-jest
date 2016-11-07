6b11143294143bb03d790e7f88bcfe82f55c7183
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-07 11:21:39 +0700

    Init starter repo

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path')
+module.exports = {
+  entry: path.resolve('src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('dist'),
+    publicPath: '/dist/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+      }
+    ],
+  }
+}
