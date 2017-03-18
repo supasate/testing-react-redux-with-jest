94f363fe22de3f973614c706620b3f26e01abfd1
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-18 18:04:44 +0700

    Setup hot reload with AppContainer

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => (
+  <div>Hello React</div>
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('Hello')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './components/App'
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('react-root')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  })
+}
