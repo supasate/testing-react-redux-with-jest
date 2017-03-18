3bfb22f9fb934807a32735ce833be02d02954304
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-19 02:40:54 +0700

    Use decorator

--- src/components/__stories__/TodoItem.stories.js
+++ src/components/__stories__/TodoItem.stories.js
@@ -5,6 +5,11 @@ import TodoItem from '../TodoItem'
 
 storiesOf('TodoItem', module)
   .addDecorator(withKnobs)
+  .addDecorator(story => (
+    <div style={{ textAlign: 'center' }}>
+      {story()}
+    </div>
+  ))
   .addWithInfo('not complete', 'Show a todo item when complete prop is blank or false', () => (
     <TodoItem text={text('Text', 'Not complete')} />
   ))
