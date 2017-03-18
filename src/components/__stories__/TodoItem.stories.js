b7d801c3d82543400bc440032c56e4f09926c3f0
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-19 02:05:30 +0700

    Use knobs addon

--- src/components/__stories__/TodoItem.stories.js
+++ src/components/__stories__/TodoItem.stories.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
+import { withKnobs, text, boolean } from '@kadira/storybook-addon-knobs';
 import TodoItem from '../TodoItem'
 
 storiesOf('TodoItem', module)
+  .addDecorator(withKnobs)
   .add('not complete', () => (
-    <TodoItem text="Not complete" />
+    <TodoItem text={text('Text', 'Not complete')} />
   ))
   .add('complete', () => (
-    <TodoItem text="Complete" complete />
+    <TodoItem text={text('Text', 'Complete')} complete={boolean('Complete', true)} />
   ))
