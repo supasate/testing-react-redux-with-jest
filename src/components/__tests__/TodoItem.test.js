70408844a816f42302236ba2ff6010eff139b637
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-19 21:11:05 +0700

    Use serializer

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import toJson from 'enzyme-to-json'
 import TodoItem from '../TodoItem'
 
 describe('TodoItem', () => {
   it('matches its snapshot - not complete', () => {
     const component = shallow(<TodoItem text="Hello" />)
 
-    expect(toJson(component)).toMatchSnapshot()
+    expect(component).toMatchSnapshot()
   })
 
   it('matches its snapshot - complete', () => {
     const component = shallow(<TodoItem text="Hello" complete />)
 
-    expect(toJson(component)).toMatchSnapshot()
+    expect(component).toMatchSnapshot()
   })
 
   it('renders correct structure', () => {
     const component = shallow(<TodoItem text="Hello" />)
 
     expect(component.is('li')).toBe(true)
+    expect(component.text()).toBe('Hello')
   })
 
   it('contains complete class if receiving complete prop', () => {
