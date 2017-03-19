8f4e891c8cc8d24304b2e49c93885a2742387356
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-19 20:45:15 +0700

    Snapshot of complete class

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import TodoItem from '../TodoItem'
 
 describe('TodoItem', () => {
+  it('matches its snapshot - not complete', () => {
+    const component = shallow(<TodoItem text="Hello" />)
+
+    expect(toJson(component)).toMatchSnapshot()
+  })
+
+  it('matches its snapshot - complete', () => {
+    const component = shallow(<TodoItem text="Hello" complete />)
+
+    expect(toJson(component)).toMatchSnapshot()
+  })
+
   it('renders correct structure', () => {
     const component = shallow(<TodoItem text="Hello" />)
 
