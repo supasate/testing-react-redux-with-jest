0efe1dde94262408d3a1660ece99f61c2fcf04ed
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-19 21:24:45 +0700

    Use beforeEach

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -3,27 +3,29 @@ import { shallow } from 'enzyme'
 import TodoItem from '../TodoItem'
 
 describe('TodoItem', () => {
-  it('matches its snapshot - not complete', () => {
-    const component = shallow(<TodoItem text="Hello" />)
+  let component
 
+  beforeEach(() => {
+    component = shallow(<TodoItem text="Hello" />)
+  })
+
+  it('matches its snapshot - not complete', () => {
     expect(component).toMatchSnapshot()
   })
 
   it('matches its snapshot - complete', () => {
-    const component = shallow(<TodoItem text="Hello" complete />)
+    component.setProps({ complete: true })
 
     expect(component).toMatchSnapshot()
   })
 
   it('renders correct structure', () => {
-    const component = shallow(<TodoItem text="Hello" />)
-
     expect(component.is('li')).toBe(true)
     expect(component.text()).toBe('Hello')
   })
 
   it('contains complete class if receiving complete prop', () => {
-    const component = shallow(<TodoItem text="Hello" complete />)
+    component.setProps({ complete: true })
 
     expect(component.hasClass('complete')).toBe(true)
   })
