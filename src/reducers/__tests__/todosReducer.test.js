af02988c0428e3b6a008b895da5dd2bc7fb434da
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-19 23:22:32 +0700

    Reducer handles ADD_TODO

--- src/reducers/__tests__/todosReducer.test.js
+++ src/reducers/__tests__/todosReducer.test.js
@@ -1,4 +1,5 @@
 import todosReducer from '../todosReducer'
+import { ADD_TODO } from '../../actions/types'
 
 describe('todosReducer', () => {
   it('returns correct initial state', () => {
@@ -12,5 +13,22 @@ describe('todosReducer', () => {
     const nextState = todosReducer(curState, {})
     expect(nextState).toBe(curState) // compare pointers
     expect(nextState).toEqual(curState) // compare values
+  })
+
+  it('adds todos when receiving ADD_TODO action', () => {
+    const curState = [
+      { id: 1, text: 'Feed cat', complete: false },
+    ]
+    const action = {
+      type: ADD_TODO,
+      payload: { text: 'Sleep' },
+    }
+    const nextState = todosReducer(curState, action)
+    const expectedState = [
+      { id: 1, text: 'Feed cat', complete: false },
+      { id: 2, text: 'Sleep', complete: false },
+    ]
+    expect(nextState).toEqual(expectedState)
+    expect(nextState).not.toBe(curState)
   })
 })
--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,7 +1,19 @@
+import { ADD_TODO } from '../actions/types'
+
 const initialState = []
+let nextId = 1
 
 const todosReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_TODO: {
+      nextId += 1
+
+      return [...state, {
+        id: nextId,
+        text: action.payload.text,
+        complete: false,
+      }]
+    }
     default:
       return state
   }
