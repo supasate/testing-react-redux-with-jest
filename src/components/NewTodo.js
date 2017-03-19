f2ac8d01eac070f5884c2f376ab4adbe50b374ca
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-19 23:59:23 +0700

    Test structure

---
+++ src/components/NewTodo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class NewTodo extends Component {
+
+  render() {
+    return (
+      <div className="new-todo">
+        <input />
+        <button className="btn btn-primary">Add</button>
+      </div>
+    )
+  }
+}
+
+export default NewTodo
---
+++ src/components/__tests__/NewTodo.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import NewTodo from '../NewTodo'
+
+describe('NewTodo', () => {
+  let component
+  let props
+
+  beforeEach(() => {
+    component = shallow(<NewTodo />)
+  })
+
+  it('matches its snapshot', () => {
+    expect(component).toMatchSnapshot()
+  })
+
+  it('renders correct structure', () => {
+    expect(component.is('div')).toBe(true)
+    expect(component.hasClass('new-todo')).toBe(true)
+    expect(component.find('input').length).toBe(1)
+    expect(component.find('button').length).toBe(1)
+    expect(component.find('button').first().hasClass('btn'))
+    expect(component.find('button').first().hasClass('btn-primary'))
+  })
+})
