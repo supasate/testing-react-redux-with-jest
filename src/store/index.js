11a80721af8a6f5d231e714efa9803708ce52feb
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-27 01:12:00 +0700

    Apply thunk middleware to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
-import { createStore as createReduxStore } from 'redux'
+import { createStore as createReduxStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
+
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const createStore = (initialState) => {
   const store = createReduxStore(
     rootReducer,
     initialState,
-    // eslint-disable-next-line no-underscore-dangle
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    composeEnhancers(
+      applyMiddleware(thunk)
+    )
   )
 
   return store
